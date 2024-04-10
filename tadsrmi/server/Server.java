f67af5ce6203a521ba9dbf83f89f63ee675c9864
Eduardo Afonso <edu_ardu@hotmail.com.br>
2024-04-10 13:53:02 -0300

    packageFix

---
+++ tadsrmi/server/Server.java
@@ -0,0 +1,20 @@
+package tadsrmi.server;
+
+import tadsrmi.ChatImplementation;
+import tadsrmi.ChatInterface;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            Registry registry = LocateRegistry.createRegistry(6000);
+            ChatInterface remoteObj = new ChatImplementation();
+            registry.bind("RemoteObj", remoteObj);
+            System.out.println("\uD83D\uDE80 Server listening...");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
